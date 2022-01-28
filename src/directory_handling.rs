813459c38b5ecf5645ac41c11214f0aad423e4aa
SoftAnnaLee <jazzchamp600@gmail.com>
2022-01-28 15:58:28 -0700

    Added directory checking and creation

--- src/directory_handling.rs
+++ src/directory_handling.rs
@@ -1,7 +1,21 @@
+use std::error::Error;
+use std::fs::{create_dir_all, read_dir};
+
+fn _check_and_create_directory(dir: &str) -> Result<(), Box<dyn Error>> {
+    if read_dir(&dir).is_err() {
+        create_dir_all(dir)?;
+        Ok(())
+    } else {
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn name() {
-        assert!(true);
+    fn create_directory_test() {
+        assert!(_check_and_create_directory("site/").is_ok());
     }
 }
